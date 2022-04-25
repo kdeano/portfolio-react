9465312517f8340b4925b323a341cd7284a3aa86
KYLE DEAN <b9015015@hallam.shu.ac.uk>
2022-04-25 17:52:15 +0100

    Added a link to LinkedIn in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
             <div>
               <p className="sm:flex pb-2 lg:pb-10">I am passionate about building elegant software. I specialise in creating software for clients ranging from individuals to small businesses. I am particulary focused on frontend development, but I am experienced in backend too.</p>
               <p className="sm:flex pb-2 lg:pb-10">I have studied 3 years of BSc Computer Science at Sheffield Hallam University. During my studies, I have achieved numerous high grades including firsts (70% or higher) in various modules. Languages used include: C, C++, C#, JavaScript, Java, Clojure, SQL.</p>
-              <p>If you are impressed with my work and interested in talking to me about recruitment, please send me an email or reach out to me on LinkedIn. You can find my resume attached to the left-hand side of the page, along with other useful links.</p>
+              <p>If you are impressed with my work and interested in talking to me about recruitment, please send me an email or reach out to me on <span className="text-white"><a href="https://www.linkedin.com/in/kdean7" target="_blank" rel="noopener noreferrer">LinkedIn</a></span>. You can find my resume attached to the left-hand side of the page, along with other useful links.</p>
             </div>
         </div>
       </div>
