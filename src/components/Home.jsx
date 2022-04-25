06f9c80d557d5cc0d01356e850d4d3ad80b58789
KYLE DEAN <b9015015@hallam.shu.ac.uk>
2022-04-25 17:57:07 +0100

    Added transition duration to button hover. Removed coming soon placeholder work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
         <p className="text-[#8892b0] py-4 max-w-[700px]">I'm a full-stack developer specialising in building and designing exceptional digital experiences. Currently, I'm focused on building responsive full-stack web applications.</p>
 
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-300">
           <Link className="group flex" onClick={handleClick} to="work" spy={true} smooth={true} duration={500}>
             View Work 
             <span className="group-hover:rotate-90 duration-300">
