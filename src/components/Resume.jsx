e86315dfb66ba125c9532f8355bb21296baa5d8f
KYLE DEAN <b9015015@hallam.shu.ac.uk>
2022-04-28 01:08:46 +0100

    Fixed sizing on mobile and tablet devices in resume route

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -8,11 +8,11 @@ const Resume = () => {
         {/* Personal information */}
         <h1 className="text-white text-4xl font-bold text-center">Kyle Dean</h1>
         <p className="pt-4 text-center">Chadderton, Oldham, Greater Manchester</p>
-        <p className="text-center">+44 783 281 669</p>
+        <p className="text-center">+44 783 281 669 | kyle.antony.dean@gmail.com</p>
 
         {/* Profile header */}
         <div className="w-full h-[2.5rem] mt-4 bg-pink-600">
-          <h1 className="text-white text-2xl font-bold px-2 py-1">PROFILE</h1>
+          <h1 className="text-white text-xl lg:text-2xl font-bold px-2 py-1.5 lg:py-1">PROFILE</h1>
         </div>
 
         {/* Profile bio */}
@@ -20,16 +20,16 @@ const Resume = () => {
 
         {/* Education & qualifications header */}
         <div className="w-full h-[2.5rem] mt-4 bg-pink-600">
-          <h1 className="text-white text-2xl font-bold px-2 py-1">EDUCATION & QUALIFICATIONS</h1>
+          <h1 className="text-white text-xl lg:text-2xl font-bold px-2 py-1.5 lg:py-1">EDUCATION & QUALIFICATIONS</h1>
         </div>
 
         {/* Education & qualifications bio */}
         {/* Sheffield Hallam */}
-        <h2 className="text-white text-lg text-left mt-2 px-2 font-semibold">Sheffield Hallam University | BSc Computer Science<span className="float-right">Sep 2019 - Expected 2022</span></h2>
+        <h2 className="text-white text-md lg:text-lg text-left mt-2 px-2 font-semibold">Sheffield Hallam University | BSc Computer Science<span className="block lg:flex lg:float-right">Sep 2019 - Expected 2022</span></h2>
         <p className="text-gray-200 text-left mt-3 px-2">Modules I have passed and studying include (but not limited to): Professional Experience, Databases, Mobile Development, Object-oriented Programming, Fundamentals of Programming, Algorithms and Data structures, Fundamentals of Computer architecture.</p>
 
         {/* Oldham College */}
-        <h2 className="text-white text-lg text-left mt-10 px-2 font-semibold">Oldham College | Advanced Technical Extended Diploma<span className="float-right">Sep 2017 - Jun 2019</span></h2>
+        <h2 className="text-white text-md lg:text-lg text-left mt-10 px-2 font-semibold">Oldham College | Advanced Technical Extended Diploma<span className="block lg:flex lg:float-right">Sep 2017 - Jun 2019</span></h2>
         <p className="text-left mt-3 px-2">Level 3 Application Development - Distinction, Distinction</p>
         <p className="text-left px-2">Core Maths - B</p>
         <p className="text-left px-2">Pearson Extended Project - D</p>
@@ -37,7 +37,7 @@ const Resume = () => {
         <p className="text-left mt-3 px-2">I studied two extra modules at college. Core maths was valuable in the practice of logical thinking. The Extended Project was a university standard piece of dissertation work, of which I wrote about AI, delving into machine learning; discussing the possible benefits and drawbacks associated with the technology. This gave me understanding and experience on the type of work expected at a higher level, and gave me the presentation skills needed in a workplace.</p>
 
         {/* Newman RC College */}
-        <h2 className="text-white text-lg text-left mt-10 px-2 font-semibold">Newman R.C. College<span className="float-right">Sep 2012 - Jun 2017</span></h2>
+        <h2 className="text-white text-md lg:text-lg text-left mt-10 px-2 font-semibold">Newman R.C. College<span className="block lg:flex lg:float-right">Sep 2012 - Jun 2017</span></h2>
         <p className="text-left mt-3 px-2">Achieved 9 GCSEs grades A-B, including Mathematics, English, and Computer Science.</p>
 
         {/* Key skills */}
@@ -67,7 +67,7 @@ const Resume = () => {
         <div className="w-full h-[2.5rem] mt-4 bg-pink-600">
           <h1 className="text-white text-2xl font-bold px-2 py-1">WORK EXPERIENCE</h1>
         </div>
-        <h2 className="text-white text-lg text-left mt-2 px-2 font-semibold">Bar Staff | CODE Nightclub<span className="float-right">Jun 2021 - Present</span></h2>
+        <h2 className="text-white text-md lg:text-lg text-left mt-2 px-2 font-semibold">Bar Staff | CODE Nightclub<span className="block lg:flex lg:float-right">Jun 2021 - Present</span></h2>
         <h3 className="px-2 text-gray-400">CODE is a reputable nightclub in Sheffield.</h3>
         <ul className="mt-3 ml-7 space-y-1 list-disc">
           <li>Volunteered and worked as a team to manage incoming stock during the day</li>
@@ -79,9 +79,9 @@ const Resume = () => {
         </ul>
 
         {/* Co-op Food */}
-        <h2 className="text-white text-lg text-left mt-10 px-2 font-semibold">Customer Team Member | Co-op Food<span className="float-right">Mar 2020 - Jun 2020</span></h2>
+        <h2 className="text-white text-md lg:text-lg text-left mt-10 px-2 font-semibold">Customer Team Member | Co-op Food<span className="block lg:flex lg:float-right">Mar 2020 - Jun 2020</span></h2>
         <h3 className="px-2 text-gray-400">Co-op Food is a well-known food retail business.</h3>
-        <ul className="mt-3 ml-7 space-y-1 list-disc">
+        <ul className="mt-3 lg:mt-3 ml-7 space-y-1 list-disc">
           <li>Worked as a team to manage the stock on the shop floor</li>
           <li>Listened to customer needs and used my knowledge to assist them</li>
           <li>Used the Deliveroo control panel to pick incoming orders</li>
