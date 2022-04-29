8db4e51054e2d1052d10aa926518b97682e72f0e
KYLE DEAN <b9015015@hallam.shu.ac.uk>
2022-04-29 06:02:40 +0100

    Fixed resume page social icon colours

--- src/components/NavbarResume.jsx
+++ src/components/NavbarResume.jsx
@@ -100,12 +100,12 @@ const NavbarResume = () => {
               GitHub <FaGithub size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6D9DC5]">
             <Link className="flex justify-between items-center w-full text-gray-300" to="/">
               Email <HiOutlineMail size={30} />
             </Link>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#db2777]">
             <Link className="flex justify-between items-center w-full text-gray-300" to="/resume">
               Resume <BsFillPersonLinesFill size={30} />
             </Link>
